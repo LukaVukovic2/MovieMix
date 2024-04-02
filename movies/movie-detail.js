4ce59e6aa06500acd124600fb77f145f7d495ec2
Luka Vukovic <vukovicluka33@gmail.com>
2024-04-02 22:13:27 +0200

    Improved layout stability omitting unnecessary absolute positioning

--- movies/movie-detail.js
+++ movies/movie-detail.js
@@ -1,13 +1,13 @@
 import { getMovieById, getCastInfo } from "../api/api-config.js"
 
 const movieDetail = document.querySelector('.movie-detail');
+const bgImage = document.querySelector('.bg-image');
 const movieDetailWrapper = document.querySelector('.movie-detail-wrapper');
 const castContainer = document.querySelector('.cast-container');
-const container = document.querySelector('.container');
 const movieExtraDesc = document.querySelector('.movie-extra-desc');
 const tagline = document.querySelector('.tagline');
-const genresEl = document.querySelector('.genres');
-const castEl = document.querySelector('.cast-photos');
+const genresContainer = document.querySelector('.genres-container');
+const castPhotosContainer = document.querySelector('.cast-photos-container');
 
 let movie;
 let imgEl;
@@ -18,18 +18,12 @@ const id = urlParams.get('id');
 
 const getMovie = async (id) => {
   try {
-    container.style.height = "height: calc(100vh - 100px);"
     movie = await getMovieById(id);
     if (movie) {
-      imgEl = document.createElement('div');
-      imgEl.classList.add('img-container')
-      imgEl.innerHTML = `
-        <img src="https://image.tmdb.org/t/p/original${movie.backdrop_path}" alt="movie image" class="movie-img">
-      `
-      container.insertBefore(imgEl, movieDetailWrapper);
+      bgImage.style.background = `url('https://image.tmdb.org/t/p/original${movie.backdrop_path}')`;
 
       if(movie.tagline){
-        tagline.innerHTML = `<i>${movie.tagline}</i>`;
+        tagline.innerHTML = `<i>${movie.tagline}</i><hr>`;
       }
       else{
         tagline.style.display = "none";
@@ -37,20 +31,23 @@ const getMovie = async (id) => {
 
       movieDetail.innerHTML = 
       `
-        <h2>${movie.title ? movie.title : movie.name}</h2>
+        <h2 class="no-margin">${movie.title ? movie.title : movie.name}</h2>
         <p>${new Date(movie.release_date).getFullYear()} | ${getMovieDuration(movie.runtime)} | ${movie.genres[0].name}</p>
         <p>${movie.overview}</p>
       `;
 
       if(movie.genres){
+        genresContainer.innerHTML = "<h2>Genres</h2>";
+        const genresEl = document.createElement('div');
+        genresEl.classList.add('genres')
+        
         movie.genres.forEach(genre =>{
           genresEl.innerHTML += 
-          `<div>${genre.name}</div>`;
+          `<div style="border-bottom: 1px solid grey">${genre.name}</div>`;
         })
-        movieExtraDesc.appendChild(genresEl);
+        genresContainer.appendChild(genresEl);
       }
     }
-    container.style.minHeight = imgEl.offsetHeight + 'px';
   } catch (error) {
     console.error(error);
     return null;
@@ -64,22 +61,41 @@ if(id){
     const movieWithId = await getCastInfo(id);
     castContainer.innerHTML += `Starring: `;
     if (movieWithId) {
+      castPhotosContainer.innerHTML = "<h2>Main Roles</h2>";
+      const castEl = document.createElement('div');
+      const wholeCast = document.createElement('div');
+      wholeCast.classList.add('whole-cast');
+      wholeCast.innerHTML = "Cast<br>"
+      castEl.classList.add('cast-photos')
+
       movieWithId.cast.forEach((actor,index) => {
         if(index >= 3){
-          return
+          if(index !== movieWithId.cast.length - 1){
+            wholeCast.innerHTML +=`<a href="./actor.html?id=${actor.id}">${actor.name + ', '}</a>`;
+          }
+          else{
+            wholeCast.innerHTML +=`<a href="./actor.html?id=${actor.id}">${actor.name}</a>`;
+          }
         }
-        castContainer.innerHTML +=  
-        `
-          ${index < 2 ? actor.name + ', ' : actor.name} 
-        `
-        castEl.innerHTML +=
-        `
-          <div>
-            <a href="./actor.html?id=${actor.id}">
-              <img src="https://image.tmdb.org/t/p/original${actor.profile_path}" alt="photo" class="img">
+        else{
+          castContainer.innerHTML +=  
+          `
+            ${index < 2 ? actor.name + ', ' : actor.name} 
+            `
+          castEl.innerHTML +=
+          `
+          <div style="position:relative">
+            <a href="./actor.html?id=${actor.id}" >
+              <img src="https://image.tmdb.org/t/p/original${actor.profile_path}" alt="photo" class="img" >
+              <div class="actor-name">
+                <p>${actor.name}</p>
+              </div>
             </a>
           </div>
-        `
+          `
+          castPhotosContainer.appendChild(castEl);
+        }
+        castPhotosContainer.appendChild(wholeCast);
       });
     } else {
       castContainer.innerHTML += " unknown";
@@ -88,7 +104,7 @@ if(id){
     console.error('Cast is unknown', error);
   }
 }
-
+      
 function getMovieDuration(minutes){
   if (minutes >= 60){
     hours = Math.floor(minutes / 60);
@@ -98,19 +114,5 @@ function getMovieDuration(minutes){
   else{
     mins = minutes;
     return `${mins}`;
-  }
-}
-
-window.addEventListener('resize', adjustHeight);
-
-
-function adjustHeight(){
-  if (imgEl && container && window.innerWidth > 600) {
-    movieExtraDesc.style.display = "block";
-    console.log(imgEl.offsetHeight)
-    container.style.height = imgEl.clientHeight + 'px';
-  }
-  else if(window.innerWidth <= 600){
-    movieExtraDesc.style.display = "none";
   }
 }
